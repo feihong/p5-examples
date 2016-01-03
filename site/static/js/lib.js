30d45ece8fb85332e4f4085fdc9cf744acc8a9b3
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-03 17:40:29 -0600

    Show coordinates on mouseover of canvas

--- site/static/js/lib.js
+++ site/static/js/lib.js
@@ -3,13 +3,32 @@
 (function() {
 
   var a = document.querySelector('a.show-grid')
+  var canvas = null
+  var coordsDiv = document.createElement('div')
+  coordsDiv.textContent = '(?, ?)'
+  var yOffset = null
+  var xOffset = null
+
   a.onclick = (evt) => {
     evt.preventDefault()
+
+    canvas = document.querySelector('canvas')
+
+    // Insert coordsDiv before canvas
+    canvas.parentNode.insertBefore(coordsDiv, canvas)
+
+    var rect = canvas.getBoundingClientRect()
+    xOffset = Math.round(rect.x)
+    yOffset = Math.round(rect.y)
+
+    canvas.onmousemove = showCoordinates
+    canvas.onmouseleave = () => {
+      coordsDiv.textContent = '(?, ?)'
+    }
     drawGrid()
   }
 
   function drawGrid() {
-    var canvas = document.querySelector('canvas')
     var w = canvas.clientWidth
     var h = canvas.clientHeight
     var ctx = canvas.getContext('2d')
@@ -27,6 +46,10 @@
       ctx.lineTo(w, i)
       ctx.stroke()
     }
+  }
+
+  function showCoordinates(evt) {
+    coordsDiv.textContent = `(${evt.clientX - xOffset - 1}, ${evt.clientY - yOffset - 2})`
   }
 
 })()
