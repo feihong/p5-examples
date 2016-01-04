fc1cf3f3ea4ead7c6bede92a6e22ab6ae02b2bbe
Feihong Hsu <feihong.hsu@gmail.com>
2016-01-03 23:30:17 -0600

    The hue increments slowly as you draw

--- site/draw/sketch.js
+++ site/draw/sketch.js
@@ -1,22 +1,33 @@
 'use strict';
 
+var hueValue = 180
+
 function setup() {
   createCanvas(500, 500)
   background('ivory')
+  colorMode(HSB)
 }
 
 function draw() {
-  stroke(0)
 }
 
 // Putting this code inside of draw() won't work.
 function mouseMoved() {
-  var dx = Math.abs(mouseX - pmouseX)
-  var dy = Math.abs(mouseY - pmouseY)
-  var d = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+  var dx = abs(mouseX - pmouseX)
+  var dy = abs(mouseY - pmouseY)
+  var d = sqrt(sq(dx) + sq(dy))
 
   d = (d > 20) ? 20 : d   // cap at 20
   strokeWeight(d)
 
+  hueValue = (hueValue + 0.5) % 360
+  stroke(hueValue, 80, 100)
+
   line(pmouseX, pmouseY, mouseX, mouseY)
+}
+
+function keyPressed() {
+  if (key === 'X') {
+    background('ivory')
+  }
 }
